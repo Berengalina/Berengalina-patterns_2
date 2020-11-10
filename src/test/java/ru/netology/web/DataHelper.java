57a8d5d16912afbe075712f37f3f5e0ff09c08f4
Anna Belousova <annabelousovva@gmail.com>
2020-11-10 13:48:39 +0300

    Update Test

--- src/test/java/ru/netology/web/DataHelper.java
+++ src/test/java/ru/netology/web/DataHelper.java
@@ -1,9 +1,14 @@
 package ru.netology.web;
 
-
 import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 
 import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
 
 public class DataHelper {
     private DataHelper() {}
@@ -11,12 +16,32 @@ public class DataHelper {
     public static class Registration {
         private Registration() {}
 
+        public static RequestSpecification requestSpec = new RequestSpecBuilder()
+                .setBaseUri("http://localhost")
+                .setPort(9999)
+                .setAccept(ContentType.JSON)
+                .setContentType(ContentType.JSON)
+                .log(LogDetail.ALL)
+                .build();
+
+
         public static RegistrationDto generateData(String locale) {
             Faker faker = new Faker(new Locale("ru"));
-            return new RegistrationDto(
-                    faker.name().username(),
-                    faker.internet().password(),
-                    "active"
+                given() // "дано"
+                        .spec(requestSpec) // указываем, какую спецификацию используем
+                        .body(new RegistrationDto (faker.name().username(),faker.internet().password(),"active" ) ) // передаём в теле объект, который будет преобразован в JSON
+                        .when() // "когда"
+                        .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                        .then() // "тогда ожидаем"
+                        .statusCode(200); // код 200 OK
+
+             return
+
+//
+//                     new RegistrationDto(
+//                    faker.name().username(),
+//                    faker.internet().password(),
+//                    "active"
 
             );
         }
--- src/test/java/ru/netology/web/LoginPage1.java
+++ src/test/java/ru/netology/web/LoginPage1.java
@@ -16,31 +16,11 @@ import static io.restassured.RestAssured.given;
 public class LoginPage1 {
 
 
-    public static RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .log(LogDetail.ALL)
-            .build();
-
-    @BeforeAll
-    static void setUpAll() {
-        // сам запрос
-        given() // "дано"
-                .spec(requestSpec) // указываем, какую спецификацию используем
-                .body(DataHelper.Registration.generateData("ru"))  // передаём в теле объект, который будет преобразован в JSON
-                .when() // "когда"
-                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
-                .then() // "тогда ожидаем"
-                .statusCode(200); // код 200 OK
-    }
-
     @Test
     void shouldLogin(){
         open("http://localhost:9999");
-        $("[data-test-id=login] .input__control").setValue(DataHelper.Registration.generateData("ru"));
-        $("[data-test-id=password] .input__control").setValue(DataHelper.Registration.generateData("ru"));
+        $("[data-test-id=login] .input__control").setValue(DataHelper.Registration.generateData("ru").getLogin());
+        $("[data-test-id=password] .input__control").setValue(DataHelper.Registration.generateData("ru").getPassword());
         $("[data-test-id=action-login] .button__text").click();
         $("h2").shouldHave(text("Личный кабинет")).shouldBe(visible);
     }
